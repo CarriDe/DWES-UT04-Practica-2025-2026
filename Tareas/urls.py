53b1ad78a2440e253bda9a39e1d4db54817c6de8
CarriDe <carrillodanielr@gmail.com>
2026-01-11 20:36:07 +0100

    Añadir perfil de usuario.

--- Tareas/urls.py
+++ Tareas/urls.py
@@ -5,6 +5,7 @@ from Aplicaciones import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view, name='login'),
+    path('indice/', views.indice, name='indice'),
     path('lista/', views.lista_usuarios, name='lista_usuarios'),
     path('tareas/', views.lista_tareas, name='lista_tareas'),
     path('crear/', views.crear_usuario, name='crear_usuario'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('ejercicios-alumnos/', views.ejercicios_alumnos, name='ejercicios_alumnos'),
     path('validar-tarea/<int:tarea_id>/', views.validar_tarea, name='validar_tarea'),
     path('validacion-profesor/', views.validacion, name='validacion_profesor'),
+    path('perfil/', views.perfil_usuario, name='perfil_usuario'),
 ]
